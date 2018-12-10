74a88300c7cbd323f83bf1c62b61d1d1e32120f8
bartek <Bartek.NNAAN@wp.pl>
2018-12-10 18:18:15 +0100

    host to firebase, live demo

---
+++ pub/precache-manifest.113e15f9c9bbca1bdbbb68acebbe1718.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70443e83a3d4c9938c85",
+    "url": "/static/js/main.70443e83.chunk.js"
+  },
+  {
+    "revision": "b0714f1bea0ecfc822e8",
+    "url": "/static/js/1.b0714f1b.chunk.js"
+  },
+  {
+    "revision": "70443e83a3d4c9938c85",
+    "url": "/static/css/main.5e5bfb26.chunk.css"
+  },
+  {
+    "revision": "144133f7145d29bbc8f852b555eda444",
+    "url": "/index.html"
+  }
+];
